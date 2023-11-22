477856b05bc2f57cf43deb2e6720897d63f2e045
raghasudha-779 <151545033+raghasudha-779@users.noreply.github.com>
2023-11-22 14:23:45 +0530

    Add files via upload

---
+++ day1/Datatype.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Datatype {
+public static void main(String[] args) {
+	char Browserlogo = 'f';
+	boolean isvisible = true;
+	String Browsername= "Firefox";
+	int releaseyear = 1998;
+	float Browserversion = 100.2f;
+	
+	System.out.println("Browser logo is = " +Browserlogo);
+	System.out.println("Isvisible = " +isvisible);
+	System.out.println("Browser Name is = " +Browsername);
+	System.out.println("Release year is = " +releaseyear);
+	System.out.println("Browser Version is = " +Browserversion);
+}
+}
